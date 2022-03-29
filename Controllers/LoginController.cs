f3d4c5d89d191b7e701d4434a77419177ede2422
Alihan Gedik <71148065+AlihanGedik@users.noreply.github.com>
2022-03-30 02:24:10 +0300

    Giriş ekranı hatası sebebiyle yeni proje oluşturuldu.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using BookInfo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookInfo.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string Username, string Password)
+        {
+            EntitiesTables ent = new EntitiesTables();
+            List<kullanicilar> userList = ent.kullanicilars.Where(user => user.kullanici_adi == Username && user.kullanici_sifre == Password).ToList();
+            int result = userList.Count();
+            if (result == 1)
+            {
+                return Redirect("/Home/Index");
+            }
+            else if (result == 0)
+            {
+                ViewBag.error = "Lütfen bilgilerinizi kontrol edin.";
+                return View();
+            }
+            return View();
+        }
+    }
+}
